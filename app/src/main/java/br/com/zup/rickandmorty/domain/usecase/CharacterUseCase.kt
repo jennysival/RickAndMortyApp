d21d4dc6be3b5684aa70873cfc25fc13cc39520f
jennysival <jeniffersival@gmail.com>
2022-07-08 16:13:46 -0300

    Implementar viewmodel

--- app/src/main/java/br/com/zup/rickandmorty/domain/usecase/CharacterUseCase.kt
+++ app/src/main/java/br/com/zup/rickandmorty/domain/usecase/CharacterUseCase.kt
@@ -1,10 +1,11 @@
 package br.com.zup.rickandmorty.domain.usecase
 
+import android.app.Application
 import br.com.zup.rickandmorty.data.model.CharacterResult
 import br.com.zup.rickandmorty.domain.repository.CharacterRepository
 import br.com.zup.rickandmorty.ui.viewstate.ViewState
 
-class CharacterUseCase {
+class CharacterUseCase(application: Application) {
     private val repository = CharacterRepository()
 
     suspend fun getAllCharactersNetwork(): ViewState<List<CharacterResult>>{
---
+++ app/src/main/java/br/com/zup/rickandmorty/ui/characterslist/viewmodel/CharacterListViewModel.kt
@@ -0,0 +1,31 @@
+package br.com.zup.rickandmorty.ui.characterslist.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import br.com.zup.rickandmorty.data.model.CharacterResult
+import br.com.zup.rickandmorty.domain.usecase.CharacterUseCase
+import br.com.zup.rickandmorty.ui.viewstate.ViewState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class CharacterListViewModel(application: Application): AndroidViewModel(application) {
+    private val characterUseCase = CharacterUseCase(application)
+    val characterListState = MutableLiveData<ViewState<List<CharacterResult>>>()
+
+    fun getAllCharactersNetwork(){
+        viewModelScope.launch {
+            try {
+                val response = withContext(Dispatchers.IO){
+                    characterUseCase.getAllCharactersNetwork()
+                }
+                characterListState.value = response
+            }catch (ex: Exception){
+                characterListState.value = ViewState.Error(Throwable("Não foi possível carregar a lista de personagens!"))
+            }
+        }
+    }
+
+}
