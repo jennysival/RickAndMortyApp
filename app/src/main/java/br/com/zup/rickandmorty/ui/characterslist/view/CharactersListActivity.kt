8143099d617bc68fc81032bcd7a39f2f8ac0745c
jennysival <jeniffersival@gmail.com>
2022-07-08 11:29:48 -0300

    Criar pacotes de arquitetura

---
+++ app/src/main/java/br/com/zup/rickandmorty/ui/characterslist/view/CharactersListActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.rickandmorty.ui.characterslist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.rickandmorty.R
+
+class CharactersListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_characters_list)
+    }
+}
