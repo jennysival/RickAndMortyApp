c6042da1070955a43a00c21b37dbed81572d8c67
jennysival <jeniffersival@gmail.com>
2022-07-08 14:45:00 -0300

    Implementar layout da tela de informações do personagem

---
+++ app/src/main/java/br/com/zup/rickandmorty/ui/characterinfo/view/CharacterInfoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.rickandmorty.ui.characterinfo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.rickandmorty.R
+
+class CharacterInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_info)
+    }
+}
