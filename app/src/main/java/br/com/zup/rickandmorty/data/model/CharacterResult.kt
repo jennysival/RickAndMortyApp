b3f0e185601730b8be14ef3614f3840443671f50
jennysival <jeniffersival@gmail.com>
2022-07-08 20:41:05 -0300

    adicionar parcelize novamente

--- app/src/main/java/br/com/zup/rickandmorty/data/model/CharacterResult.kt
+++ app/src/main/java/br/com/zup/rickandmorty/data/model/CharacterResult.kt
@@ -1,8 +1,11 @@
 package br.com.zup.rickandmorty.data.model
 
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class CharacterResult(
     @SerializedName("created")
     val created: String,
@@ -14,12 +17,8 @@ data class CharacterResult(
     val id: Int,
     @SerializedName("image")
     val image: String,
-    @SerializedName("location")
-    val location: Location,
     @SerializedName("name")
     val name: String,
-    @SerializedName("origin")
-    val origin: Origin,
     @SerializedName("species")
     val species: String,
     @SerializedName("status")
@@ -28,4 +27,4 @@ data class CharacterResult(
     val type: String,
     @SerializedName("url")
     val url: String
-)
+):Parcelable
