0b930a6e08f0c24753e5dc42f151639a557fac20
jennysival <jeniffersival@gmail.com>
2022-07-09 12:05:32 -0300

    Implementar database

--- app/src/main/java/br/com/zup/rickandmorty/data/datasource/local/CharacterDatabase.kt
+++ app/src/main/java/br/com/zup/rickandmorty/data/datasource/local/CharacterDatabase.kt
@@ -1,9 +1,36 @@
 package br.com.zup.rickandmorty.data.datasource.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import br.com.zup.rickandmorty.data.datasource.local.dao.CharacterDao
 import br.com.zup.rickandmorty.data.model.CharacterResult
 
 @Database(entities = [CharacterResult::class], version = 1)
 abstract class CharacterDatabase: RoomDatabase() {
+
+    abstract fun characterDao(): CharacterDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: CharacterDatabase? = null
+
+        fun getCharacterDatabase(context: Context): CharacterDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CharacterDatabase::class.java,
+                    "character_database"
+                ).fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
