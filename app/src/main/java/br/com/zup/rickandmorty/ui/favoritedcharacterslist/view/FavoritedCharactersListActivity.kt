c69057b4a14a68f52ee097786c4378698b68a46c
jennysival <jeniffersival@gmail.com>
2022-07-09 20:36:47 -0300

    Implementar layout da página de favoritos

---
+++ app/src/main/java/br/com/zup/rickandmorty/ui/favoritedcharacterslist/view/FavoritedCharactersListActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.rickandmorty.ui.favoritedcharacterslist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.rickandmorty.R
+
+class FavoritedCharactersListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorited_characters_list)
+    }
+}
