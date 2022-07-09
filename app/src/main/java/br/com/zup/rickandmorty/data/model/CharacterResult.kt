e06442374999a24e6edd7eb6a93804ee582f1d54
jennysival <jeniffersival@gmail.com>
2022-07-09 11:54:36 -0300

    Implementar entidade

--- app/src/main/java/br/com/zup/rickandmorty/data/model/CharacterResult.kt
+++ app/src/main/java/br/com/zup/rickandmorty/data/model/CharacterResult.kt
@@ -2,13 +2,20 @@ package br.com.zup.rickandmorty.data.model
 
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity(tableName = "character_table")
+
 data class CharacterResult(
+
     @SerializedName("id")
+    @PrimaryKey(autoGenerate = false)
     var id: Int = 0,
+
     @SerializedName("created")
     var created: String = "",
     @SerializedName("gender")
