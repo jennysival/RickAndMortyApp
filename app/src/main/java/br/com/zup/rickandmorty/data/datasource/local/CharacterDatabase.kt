846f943315ed2bc94617a0e17fed86a4b65b786b
jennysival <jeniffersival@gmail.com>
2022-07-09 22:51:53 -0300

    Atualizar a versão do database

--- app/src/main/java/br/com/zup/rickandmorty/data/datasource/local/CharacterDatabase.kt
+++ app/src/main/java/br/com/zup/rickandmorty/data/datasource/local/CharacterDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import br.com.zup.rickandmorty.data.datasource.local.dao.CharacterDao
 import br.com.zup.rickandmorty.data.model.CharacterResult
 
-@Database(entities = [CharacterResult::class], version = 2)
+@Database(entities = [CharacterResult::class], version = 3)
 abstract class CharacterDatabase: RoomDatabase() {
 
     abstract fun characterDao(): CharacterDao
